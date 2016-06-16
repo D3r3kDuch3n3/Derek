638bcb6ecedc584821b434c62e26d3fe30b9ddb8
D3r3kDuch3n3 <phoenixgames.real@gmail.com>
2016-06-15 19:24:46 -0500

    put one robot

---
+++ Derek.java
@@ -0,0 +1,56 @@
+package Derek;
+import robocode.*;
+import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Derek - a robot by (Derek)
+ */
+public class Derek extends Robot
+{
+	/**
+	 * run: Derek's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		 setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(600);
+			turnGunRight(360);
+			turnRight(90);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: Shoot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
